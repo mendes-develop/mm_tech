4f6cadcc21384d9819be81cb0bf69f229ef1f8ca
Alex Mendes <46525527+mendes-develop@users.noreply.github.com>
2024-08-01 21:39:25 -0400

    chore: add monolith swagger package

--- apps/monolith/src/index.ts
+++ apps/monolith/src/index.ts
@@ -1,9 +1,23 @@
+import { swaggerUI } from '@hono/swagger-ui'
 import { Hono } from 'hono'
 
 const app = new Hono()
 
+app.get('/ui', swaggerUI({ url: '/doc', version: '3.0.0' }))
+
+
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+
+app.get('/:name', (c) => {
+  console.log(c.req.param)
+  return c.text(`Hello ${c.req.param('name')}!`)
+})
+
+app.get('/:name/:age', (c) => {
+  return c.text(`Hello ${c.req.param("name")} ${c.req.param("age")}!`)
 })
 
 export default app
